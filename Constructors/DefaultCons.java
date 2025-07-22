ca39a7d1792b1646ab71dc0e86dc984db64d6e0a
Saloni Tomar <tomarsaloni19@gmail.com>
2025-07-22 14:48:43 +0530

    Adding one Type of constructor(Default Constructor)

---
+++ Constructors/DefaultCons.java
@@ -0,0 +1,12 @@
+package Constructors;
+
+public class DefaultCons {
+
+    DefaultCons() {
+        System.out.println("Calling Default Constructor");
+    }
+    
+    public static void main(String args[]){
+        DefaultCons Cons = new DefaultCons();
+    }
+}
